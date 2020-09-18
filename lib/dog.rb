01c7787d918eb779f687ebd7134152c435590cea
BarbaraKuofie <barbara.kuofie@gmail.com>
2020-09-17 21:15:12 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,28 +2,34 @@
 require 'pry'
 
 class Dog  
- attr_accessor :name 
-
+ attr_accessor :name
+## this is a class variable 
     @@all = [] 
 
+    ## these are class methods that gets called on every dog instance/initialization
     def initialize(name)
         @name = name 
         @@all << self 
         @save = save 
     end 
 
+    # this will return all the dog instances 
     def self.all 
        @@all
     end 
      
+    # .clear deletes everything in this array
     def self.clear_all 
         @@all.clear 
     end 
 
+# this calls the @@all class variable and for each, puts out name
     def self.print_all 
+        @@all.each do |dog|
+            puts dog.name 
+        end 
     end 
 
-    def save 
-        @save 
+    def save
     end 
 end 
